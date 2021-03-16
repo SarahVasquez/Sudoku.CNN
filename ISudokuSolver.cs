6967266d4beb46c7f95180f9c7c679b96dccbb15
SarahVasquez <74977367+SarahVasquez@users.noreply.github.com>
2021-03-16 20:56:48 +0100

    Create ISudokuSolver.cs

---
+++ ISudokuSolver.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku.Core
+{
+    public interface ISudokuSolver
+    {
+
+        void Solve(GrilleSudoku s);
+        
+    }
+}
